c5236ec305718b5f364baa47eef494ba1e1238aa
Zeonx9 <mushtukov.artem@mail.ru>
2023-01-16 00:45:05 +0300

    add tests to .services package and .repositories package

---
+++ src/test/java/com/ade/chat/repositories/UserRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.ade.chat.repositories;
+
+import com.ade.chat.entities.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+@DataJpaTest
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+    }
+
+    @Test
+    void itShouldFindTheStudentByName() {
+        // given
+        String name = "Artem";
+        User user = new User(name);
+        underTest.save(user);
+
+        // when
+        Optional<User> foundUser = underTest.findByName(name);
+
+        // then
+        assertThat(foundUser).isPresent();
+        assertThat(foundUser.get()).isEqualTo(user);
+
+    }
+
+    @Test
+    void itShouldCheckTheExistenceOfUserByName() {
+        // given
+        User user = new User("Artem");
+        underTest.save(user);
+
+        //when
+        Boolean result = underTest.hasUserWithName(user.getName());
+
+        //then
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void itShouldCheckTheAbsenceOfUserByName() {
+        // given
+        User user = new User("Artem");
+
+        //when
+        Boolean result = underTest.hasUserWithName(user.getName());
+
+        //then
+        assertThat(result).isFalse();
+    }
+
+}
