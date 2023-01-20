c0dbf1f932eb15ba058239fb9894ad55805887a0
Zeonx9 <mushtukov.artem@mail.ru>
2023-01-21 01:06:44 +0300

    get rid of queries in repositories

--- src/main/java/com/ade/chat/repositories/ChatRepository.java
+++ src/main/java/com/ade/chat/repositories/ChatRepository.java
@@ -2,17 +2,7 @@ package com.ade.chat.repositories;
 
 import com.ade.chat.entities.Chat;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-
-    @Query(
-            "select c from chat c where c.isPrivate=true and " +
-            "not exists (select 1 from c.members m where m.id not in ?1)"
-    )
-    Optional<Chat> findPrivateByMemberIds(List<Long> ids);
 
 }
