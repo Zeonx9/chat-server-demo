0d71437740959a43bec7b013bfa97192e03e8f8f
Zeonx9 <mushtukov.artem@mail.ru>
2024-05-02 19:49:25 +0700

    allow all origins for stomp endpoint

--- src/main/java/com/ade/chat/config/WebSocketConfig.java
+++ src/main/java/com/ade/chat/config/WebSocketConfig.java
@@ -19,6 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws-endpoint")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
